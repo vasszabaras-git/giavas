d413efcd77b698cd6a32ebbf20c293cfbaba99f8
vasszabaras-git <vass.zabaras@gmail.com>
2025-12-07 19:46:19 +0100

    Delete src directory

--- src/Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello bitch! i dont lik ethtis i change it to stthhh");
-        System.out.println("new changes are made here");
-
-        System.out.println("hferifr");
-    }
-}
--- src/hello2.java
+++
@@ -1,7 +0,0 @@
-public class hello2 {
-
-    public static void main() {
-
-        System.out.println("Hello 22");
-    }
-}
