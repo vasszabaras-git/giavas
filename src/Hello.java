42c4b78328a37f303ed442fb186f24e0d36acfca
vzamparas <vass.zabaras@gmail.com>
2025-12-07 19:23:41 +0100

    fhriur

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello bitch!");
+        System.out.println("Hello bitch! i dont lik ethtis i change it to stthhh");
+        System.out.println("new changes are made here");
+
+        System.out.println("hferifr");
     }
 }
